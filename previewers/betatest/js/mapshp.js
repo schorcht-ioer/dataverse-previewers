04418d3a49b5b3984caa1a320d9506f39e54f9c4
Martin Schorcht <123582538+schorcht-ioer@users.noreply.github.com>
2026-07-21 15:21:11 +0200

    Refactor file size handling and increase limit to 50 MB
    Refactored file size checking logic to use metadata and increased file size limit to 50 MB

--- previewers/betatest/js/mapshp.js
+++ previewers/betatest/js/mapshp.js
@@ -11,43 +11,77 @@ function translateBaseHtmlPage() {
 }
 
 // set limits
-const file_size_limit = 20; // in MB
+const fileSizeLimit = 50; // in MB
 
 // enable spinner
 var target = document.getElementById('map');
 var spinner = new Spinner().spin(target);
 
-function writeContent(fileUrl, file, title, authors) {
+async function loadMetadata(metadataUrl) {
+  const response = await fetch(metadataUrl);
+
+  if (!response.ok) {
+    throw new Error(`HTTP Error: ${response.status}`);
+  }
+
+  const metadata = await response.json();
+
+  return metadata;
+}
+
+async function getFileSize(){
+    
+    // size via header (replaced with metadata...)
+    //  fetch(fileUrl, { method: "HEAD" })
+    //    .then(r => console.log(r.headers.get("Content-Length")));
+
+    const fileid = queryParams.fileid;
+    const datasetMetadataUrl = queryParams.versionUrl;
+
+    // metadata of dataset
+    const metadata = await loadMetadata(datasetMetadataUrl);
+
+    // metadata of all files including name and id
+    const filesMetadata = metadata.data.files;
+
+    // metadata of tiff what to preview
+    const fileMetadata = filesMetadata.find(f => f.dataFile.id === fileid);
+
+    // get file name and size (mb) of tiff
+    const fileName = fileMetadata.dataFile.filename;
+    const fileSize = Math.round(fileMetadata.dataFile.filesize/(1024**2));
+    console.log(fileName, fileSize);
+
+    return fileSize
+}
+
+async function writeContent(fileUrl, file, title, authors) {
     addStandardPreviewHeader(file, title, authors);
 
-    //check file size
-    const url_to_file_info = fileUrl.replace("access/data","").replace("file","files");
+    const fileSize = await getFileSize();   
 
-    $.getJSON(url_to_file_info, function( data ) {
-        const file_size = data.data.dataFile.filesize/(1024**2);
+    if (fileSize > fileSizeLimit){
+        show_error(`The file is too big to be displayed (limit is ${fileSizeLimit.toString()} MB)`);
+    }else{
+        // load a tile layer
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-        if (file_size > file_size_limit){
-            show_error(`The file is too big to be displayed (limit is ${file_size_limit.toString()} MB)`);
-        }else{
-            // load a tile layer
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
-
-            // get data
-            var request = new XMLHttpRequest();
-            request.open('GET', fileUrl, true);
-            request.responseType = 'blob';
-            request.onload = function() {
-                var reader = new FileReader();
-                reader.readAsArrayBuffer(request.response);
-                reader.onload =  function(e){
-                    convertToLayer(e.target.result);        
-                };
+        // get data
+        var request = new XMLHttpRequest();
+        request.open('GET', fileUrl, true);
+        request.responseType = 'blob';
+        request.onload = function() {
+            var reader = new FileReader();
+            reader.readAsArrayBuffer(request.response);
+            reader.onload =  function(e){
+                convertToLayer(e.target.result);        
             };
-            request.send();
-        }
-    });
+        };
+        request.send();
+    }
+
 } 
 
 function convertToLayer(buffer){
